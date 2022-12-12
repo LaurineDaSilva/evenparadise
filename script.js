21f2482cba74b80c36ead251ac1c77364fbc04cb
LaurineDS <laurine.dasilva0@gmail.com>
2022-12-12 17:41:03 +0100

    wip tootips

--- script.js
+++ script.js
@@ -19,36 +19,37 @@ date.setAttribute("min", today);
 
 const form = document.querySelector("form");
 const elements = form.elements;
-
-
-
-
-
-
+let tooltip;
 
 for (const element of elements) {
+
     element.addEventListener('invalid', (event) => {
         event.preventDefault();
+
         element.classList.add("is-invalid");
+        
         const invalidElements = document.getElementsByClassName("is-invalid");
         const firstInvalidElement = invalidElements[0];
-        const tooltip = new bootstrap.Tooltip(firstInvalidElement);
-        if ((element.type == "date"))  {
-            tooltip.setContent({ '.tooltip-inner': 'DATE' });
-        } else if ((element.type == "number"))  {
-            tooltip.setContent({ '.tooltip-inner': 'MONTANT' });
+        tooltip = new bootstrap.Tooltip(element);
+
+        if ((element.type == "date") && (element.value < today))  {
+            tooltip.setContent({ '.tooltip-inner': "Doit être égale ou supérieure à aujourd'hui" });
+        } else if ((element.type == "number") && (element.value <= 0))  {
+            tooltip.setContent({ '.tooltip-inner': 'Doit être positif' });
         };
+
         firstInvalidElement.focus();
      });
+
     element.addEventListener('change', (event) => {
         event.preventDefault();
         if (((element.type == "date") && (element.value < today)) || (element.value == "") || ((element.type == "number") && (element.value <= 0))) {
             element.classList.add("is-invalid");
-            const tooltip = new bootstrap.Tooltip(element);
-            if ((element.type == "date")) {
-                tooltip.setContent({ '.tooltip-inner': 'DATE' });
-            } else if ((element.type == "number")) {
-                tooltip.setContent({ '.tooltip-inner': 'MONTANT' });
+            tooltip = new bootstrap.Tooltip(element);
+            if ((element.type == "date" && (element.value < today))) {
+                tooltip.setContent({ '.tooltip-inner': "Doit être égale ou supérieure à aujourd'hui" });
+            } else if ((element.type == "number") && (element.value <= 0)) {
+                tooltip.setContent({ '.tooltip-inner': 'Doit être positif' });
             };
         } else {
             element.classList.remove("is-invalid");
