864f2663c084fc7f1326bd7d904813e8a340b64e
LaurineDS <laurine.dasilva0@gmail.com>
2023-02-23 23:47:20 +0100

    backend: fixed issues of run app; added post to controller. Post works but get all does not. Issue with events place, theme and rate in controller (post), need to check why and fix it

--- eventparadise-api/src/main/java/co/simplon/eventparadise/EventparadiseApiApplication.java
+++ eventparadise-api/src/main/java/co/simplon/eventparadise/EventparadiseApiApplication.java
@@ -2,12 +2,15 @@ package co.simplon.eventparadise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	DataSourceAutoConfiguration.class })
 public class EventparadiseApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventparadiseApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(
+		EventparadiseApiApplication.class, args);
+    }
 
 }
--- eventparadise-api/src/main/java/co/simplon/eventparadise/controllers/EventController.java
+++ eventparadise-api/src/main/java/co/simplon/eventparadise/controllers/EventController.java
@@ -3,11 +3,17 @@ package co.simplon.eventparadise.controllers;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.simplon.eventparadise.database.Database;
+import co.simplon.eventparadise.dtos.EventCreateDto;
 import co.simplon.eventparadise.dtos.EventView;
 import co.simplon.eventparadise.entities.Event;
 
@@ -16,6 +22,7 @@ import co.simplon.eventparadise.entities.Event;
 @CrossOrigin
 public class EventController {
 
+    @GetMapping
     public Collection<EventView> getAll() {
 	Collection<Event> events = Database.findAll();
 	Collection<EventView> views = new ArrayList<>();
@@ -30,5 +37,19 @@ public class EventController {
 	    view.setDescription(event.getDescription());
 	}
 	return views;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void create(@RequestBody EventCreateDto inputs) {
+	Event event = new Event();
+	event.setName(inputs.getName());
+	event.setDate(inputs.getDate());
+	// event.setPlace(inputs.getPlaceId());
+	// event.setTheme(inputs.getThemeId);
+	// event.setRate(inputs.getRate);
+	event.setDescription(inputs.getDescription());
+	Database.saveEvent(event);
+	System.out.println(event);
     }
 }
